76db0d6739baec52b0275362213bccb4e5d60159
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2024-01-15 16:14:23 +0530

    module and component paths successfully included

--- HotelBooking/src/app/app-routing.module.ts
+++ HotelBooking/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LandingComponent},
+  {path:'landing', component:LandingComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signUp', component:SignUpComponent},
+  {path:'admin', loadChildren:()=>import('./admin/admin.module').then(m=>m.AdminModule)},
+  {path:'owner', loadChildren:()=>import('./owner/owner.module').then(m=>m.OwnerModule)},
+  {path:'user', loadChildren:()=>import('./user/user.module').then(m=>m.UserModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- HotelBooking/src/app/app.module.ts
+++ HotelBooking/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
